3a021dc2edff8444c84b12d30208b5604dcff1e3
rendoruiz <rendo.ruiz@outlook.com>
2021-09-11 16:39:26 -0600

    nav pt2 mobile

---
+++ ninjafood/public/index.js
@@ -0,0 +1,10 @@
+const toggle = document.getElementById('toggle');
+const menu = document.getElementById('menu');
+
+toggle.addEventListener('click', () => {
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+  } else {
+    menu.classList.add('hidden');
+  }
+})
